c47de772722dd414200b9ba9d6939319eaf54390
safran6ix <mohammedsafran6ix@gmail.com>
2025-02-28 14:50:49 +0530

    Modified

--- admin panel with mini Database - suc/script.js
+++ admin panel with mini Database - suc/script.js
@@ -1,15 +1,15 @@
 // Sample user database
 const users = {
-    "codify": {
-        username: "codify",
+    "Codify": {
+        username: "Codify",
         password: "12345",
         name: "Codify6ix",
         accountNumber: "123-456-789",
         balance: 1000,
         address: "253f, lorem the hype, deth ruin diese."
     },
-    "user2": {
-        username: "user2",
+    "User2": {
+        username: "User2",
         password: "123456",
         name: "Jane Smith",
         accountNumber: "987-654-321",
