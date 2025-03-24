37ca159deb17a39715ff50238d3d8e165db9c7a9
safran6ix <mohammedsafran6ix@gmail.com>
2025-03-25 02:27:38 +0530

    Modified

--- homepage.js
+++ homepage.js
@@ -1,5 +1,7 @@
 //HOMEPAGE_HTML_SCRIPT|-
 const sidenavbar = document.querySelector(".side-navbar");
+const body = document.getElementById('body');
+const title = document.querySelector(".title");
 
 function show_navbar() {
     sidenavbar.style.left = "0";
@@ -7,4 +9,12 @@ function show_navbar() {
 
 function none_show_navbar() {
     sidenavbar.style.left = "-800px";
+}
+
+function lightON(){
+    body.style.background = "white";
+    title.style.color = "black";
+}
+function lightOFF(){
+    body.style.background = "black";
 }
