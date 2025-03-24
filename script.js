a0a39ebc95427268b3a40d775e2d9b5ab6beb209
safran6ix <mohammedsafran6ix@gmail.com>
2025-03-25 01:31:10 +0530

    JS Modified

--- script.js
+++ script.js
@@ -81,3 +81,12 @@ logoutBtn.addEventListener("click", logout);
 
 
 //HOMEPAGE_HTML_SCRIPT|-
+const sidenavbar = document.querySelector(".side-navbar");
+
+function show_navbar(){
+    sidenavbar.style.left = "0";
+}
+
+function none_show_navbar(){
+    sidenavbar.style.left = "-800px";
+}
